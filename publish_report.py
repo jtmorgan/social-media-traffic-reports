cb202786a184901ff0de2af8cd78a66317553e3b
jtmorgan <jonnymorgan.esq@gmail.com>
2020-03-18 14:01:27 -0700

    reduce hardcoded output params

--- publish_report.py
+++ publish_report.py
@@ -10,9 +10,6 @@ from requests_oauthlib import OAuth1
 ##TODO
 #percent change d-o-d column
 #>30, >100? instaed of zeros
-#categories
-#link to About page
-#print to first page section
 
 #instantiate authentication
 auth1 = OAuth1(config.consumer_key,
@@ -20,7 +17,7 @@ auth1 = OAuth1(config.consumer_key,
                config.access_key,
                config.access_secret)
 
-#page template (top)
+#page sectiona and wikitable template (top)
 rt_header = """== Top articles by social media traffic on {year}-{month}-{day} ==
 Last updated on ~~~~~
 
@@ -53,20 +50,17 @@ def prepare_data(df_traffic):
     Returns the full wikipage text as a string
     """
 
-    #sort by descending views
+    #sort dataframe by views this platform yesterday (descending)
     df_traffic.sort_values('smtpageviews', ascending=False, inplace=True)
 
-    #create a new set of ranks
+    #rank rows by views from this platform yesterday
     new_rank = range(1, len(df_traffic)+1)
-
-    #swap in the new value in the 'Rank' column
     df_traffic['rank'] = list(new_rank)
 
-    #re-zero the index
+    #reset the index to reflect the ranking
     df_traffic.reset_index(drop=True, inplace=True)
 
-    #format the individual table rows with the data
-    #from each row of the dataframe, preserving the order
+    #format the wikitable rows with relevant columns from the dataframe
     report_rows = [format_row(a, b, c, d, e, f, g, h, rt_row)
     for a, b, c, d, e, f, g, h
     in zip(df_traffic['rank'],
@@ -82,26 +76,27 @@ def prepare_data(df_traffic):
     #join this list into a single string
     rows_wiki = ''.join(report_rows)
 
+    #get datestrings from yesterday and the day before to populate the wikitable
     date_parts = {'year': datetime.strftime(datetime.now() - timedelta(1), '%Y'),
    'month' : datetime.strftime(datetime.now() - timedelta(1), '%m'),
    'day': datetime.strftime(datetime.now() - timedelta(1), '%d'),
    'month2' : datetime.strftime(datetime.now() - timedelta(2), '%m'),
    'day2': datetime.strftime(datetime.now() - timedelta(2), '%d'),
         }
-
+    #format wikipage header template
     header = rt_header.format(**date_parts)
 
     #combine the header with the now-populated table rows
     output = header + rows_wiki + "|}"
 
-    print(output)
+#     print(output)
     return(output)
 
 
 def format_row(rank, platform, title, p_views, p_views_y, t_views, watch, v_watch, row_template):
     """
-    Accepts an iterator from the dataframe with data from each row
-    Returns the rows formatted per the wikitext table row template
+    Accepts column values from the dataframe with data from each row
+    Returns a row formatted per the wikitext table row template
     """
 
     table_row = {'rank': rank,
@@ -125,19 +120,18 @@ def get_token(auth1):
     """
 
     result = requests.get(
-        url="https://test.wikipedia.org/w/api.php", #TODO add to config
+        url = config.url,
         params={
             'action': "query",
             'meta': "tokens",
             'type': "csrf",
-            'format': "json"
+            'format': "json",
             },
-        headers={'User-Agent': "jmorgan@wikimedia.org"}, #TODO add to config
+        headers={'User-Agent': config.user_agent},
         auth=auth1,
         ).json()
 
-    print(result)
-
+#     print(result)
     edit_token = result['query']['tokens']['csrftoken']
 
     return(edit_token)
@@ -147,22 +141,22 @@ def publish_report(output, auth1, edit_token):
     Accepts the page text, credentials and edit token
     Publishes the formatted page text to the specified wiki
     """
+
     response = requests.post(
-    url = "https://test.wikipedia.org/w/api.php", #TODO add to config
+    url = config.url,
     data={
         'action': "edit",
-        'title': "User:Jmorgan_(WMF)/sandbox", #TODO add to config
-#         'section': "new",
-        'summary': "testing publish_report.py", #TODO add to config
+        'title': config.page_title,
+        'section': config.edit_sec,
+        'summary': "testing publish_report.py", #TODO pass in report dates
         'text': output,
         'bot': 0,
         'token': edit_token,
-        'format': "json"
+        'format': "json",
         },
-    headers={'User-Agent': "jmorgan@wikimedia.org"}, #TODO add to config
+    headers={'User-Agent': config.user_agent},
     auth=auth1
         )
-
 #     print(response)
 
 if __name__ == "__main__":
